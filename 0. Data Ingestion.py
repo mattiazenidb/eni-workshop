0930616470826657f939a0657f064a9d3cf35799
mattiazenidb <mattiazenidb>
2024-03-12 19:28:25 +0000

    Editing 0 and 6

--- 0. Data Ingestion.py
+++ 0. Data Ingestion.py
@@ -1,4 +1,4 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC
-# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/dataingestion.png" style="float:right; margin-left: 10px" />
+# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/dataingestion.gif" style="float:right; margin-left: 10px" />
