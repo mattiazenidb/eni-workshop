11a795d900c522986247d2c266de7cedb3df153c
mattiazenidb <mattiazenidb>
2024-03-12 19:03:28 +0000

    Adding placeholder notebooks and gif

---
+++ 0. Data Ingestion.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/dataingestion.png" style="float:right; margin-left: 10px" />
