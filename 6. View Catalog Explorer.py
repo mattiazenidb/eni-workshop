31d31021c045b4719c45fde3fdcdda62599117c6
mattiazenidb <mattiazenidb>
2024-03-12 19:33:48 +0000

    Updated gif

--- 6. View Catalog Explorer.py
+++ 6. View Catalog Explorer.py
@@ -1,4 +1,4 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC
-# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/explorer.gif" style="float:right; margin-left: 10px" />
+# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/explorer_2.gif" style="float:right; margin-left: 10px" />
