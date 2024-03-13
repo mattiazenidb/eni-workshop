3db178bd3a11988002df04c8649bdebb82224503
mattiazenidb <mattiazenidb>
2024-03-13 21:12:21 +0000

    Update gif 1

--- 1. Data Ingestion.py
+++ 1. Data Ingestion.py
@@ -7,4 +7,4 @@
 
 # MAGIC %md
 # MAGIC
-# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/dataingestion.gif" style="float:right; margin-left: 10px" />
+# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/dataingestion_2.gif" style="float:right; margin-left: 10px" />
