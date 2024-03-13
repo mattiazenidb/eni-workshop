ec0c0b1a17f193e19ed0944d2c6f7dc091aae0b4
mattiazenidb <mattiazenidb>
2024-03-13 21:09:56 +0000

    Updated gif for 0

--- 10. Exploring Databricks ML.py
+++ 10. Exploring Databricks ML.py
@@ -7,9 +7,3 @@
 
 # MAGIC %md
 # MAGIC
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC
-# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/powerbi_2.gif" style="float:right; margin-left: 10px" />
