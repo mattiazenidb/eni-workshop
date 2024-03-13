50bc8d5c367fcc212f7f0f6fff77120838490906
mattiazenidb <mattiazenidb>
2024-03-13 18:07:54 +0000

    Finished first version of 1

--- 1. Process data from Volumes.py
+++ 1. Process data from Volumes.py
@@ -22,13 +22,13 @@ current_user = json.loads(dbutils.notebook.entry_point.getDbutils().notebook().g
 
 # MAGIC %md
 # MAGIC
-# MAGIC ## Databricks has native capabilities to read a multitude of formats.
+# MAGIC ## Databricks has native capabilities to read a multitude of formats: delta, csv, xml, json, avro, parquet, etc etc
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC
-# MAGIC ## Explore the data available from the Catalog Explorer on the right
+# MAGIC ## Explore the data available in the Catalog Explorer on the right
 
 # COMMAND ----------
 
@@ -38,13 +38,83 @@ current_user = json.loads(dbutils.notebook.entry_point.getDbutils().notebook().g
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC
+# MAGIC ## Read a csv file
+
+# COMMAND ----------
+
 df_iot = spark.read.csv('/Volumes/landing/power/readings/turbines')
+
+# COMMAND ----------
+
+df_iot.count()
+
+# COMMAND ----------
+
+df_iot.display()
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC
-# MAGIC ## With proper libraries it can read any format! For example, Excel files
+# MAGIC ## Read a json file instead
+
+# COMMAND ----------
+
+df_text = spark.read.text('/Volumes/landing/power/readings/engines/')
+
+# COMMAND ----------
+
+df_text.display()
+
+# COMMAND ----------
+
+df_text.printSchema()
+
+# COMMAND ----------
+
+df_json = spark.read.json('/Volumes/landing/power/readings/engines/')
+
+# COMMAND ----------
+
+df_json.display()
+
+# COMMAND ----------
+
+df_json.printSchema()
+
+# COMMAND ----------
+
+import pyspark.sql.functions as F
+
+df_json = df_json.withColumn("gender", F.when(F.col("gender") == 1, "M").otherwise("F"))
+
+# COMMAND ----------
+
+df_json.display()
+
+# COMMAND ----------
+
+df_json.printSchema()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## With proper libraries you can read any format you like! For example, Excel files
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Upload an Excel file to a Volume from your local computer
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/upload.gif" style="float:right; margin-left: 10px" />
 
 # COMMAND ----------
 
@@ -55,12 +125,12 @@ df_iot = spark.read.csv('/Volumes/landing/power/readings/turbines')
 from pandas import read_excel
 
 my_sheet = 'Sheet1' # change it to your sheet name, you can find your sheet name at the bottom left of your excel file
-file_name = '/Volumes/odl_user_1256917_catalog/default/odl_user_1256917_volume/file_example_XLS_5000.xls' # change it to the name of your excel file
+file_name = f'/Volumes/{current_user}_catalog/default/{current_user}_volume/file_example_XLS_5000.xls' # change it to the name of your excel file
 df_pandas = read_excel(file_name, sheet_name = my_sheet)
 
 # COMMAND ----------
 
-print(df_pandas.head()) # shows headers with top 5 rows
+df_pandas.head() # shows headers with top 5 rows
 
 # COMMAND ----------
 
@@ -80,16 +150,10 @@ df_bronze.display()
 
 # COMMAND ----------
 
-df.write.saveAsTable("main.default.test_table")
+# MAGIC %md
+# MAGIC
+# MAGIC ## The ultimate goal of Data Engineering is to write curated data (cleaned, verified, certified) to tables that others can use
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC
-# MAGIC ## Upload data to a Volume from your local computer
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC
-# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/upload.gif" style="float:right; margin-left: 10px" />
+df_json.write.saveAsTable(f"{current_user}_catalog.default.table_json")
