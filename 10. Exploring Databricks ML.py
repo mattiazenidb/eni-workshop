532bee9bf7bab32b469a1d71f3376cab9a8135ee
mattiazenidb <mattiazenidb>
2024-03-13 20:46:21 +0000

    ML part

---
+++ 10. Exploring Databricks ML.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC <img width="1000px" src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/ml.png" />
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/powerbi_2.gif" style="float:right; margin-left: 10px" />
