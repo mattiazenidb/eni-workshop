a7fbdbc668f3332f829542db353635093a978454
mattiazenidb <mattiazenidb>
2024-03-13 16:29:37 +0000

    Added powerbi part

---
+++ 9. Connection to PowerBI.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/powerbi.gif" style="float:right; margin-left: 10px" />
